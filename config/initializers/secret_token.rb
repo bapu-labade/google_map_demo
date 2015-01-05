42486db0b3ac144cd2facec1a383b6c2da6745e8
Bapu Labade <blabade@castlighthealth.com>
2015-01-05 10:51:19 +0530

    Implementation of geo coder google map example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleMapDemo::Application.config.secret_token = '2a5a16b9733e263f7c9645411a39e585c92e7981f5e82047f7ae6ef4118842377f09eea28b0d241e3bb3ecf338a5a71c2e444901b846e0f541457e9eadf0eaf0'
